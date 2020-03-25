c50dccf8eaf97f0b47850d7ad4d1f61ffc81219b
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-25 20:43:45 +0000

    adds seed data to console.rb

--- db/console.rb
+++ db/console.rb
@@ -3,3 +3,32 @@
 #albums contain title, genre and artist ID.
 #due to orders, artist are created first and destroyed last
 #
+
+require('pry')
+require_relative('../models/album.rb')
+require_relative('../models/artist.rb')
+
+artist1 = Artist.new({
+  'name' => 'Frightened Rabbit'
+  })
+artist1.save()
+
+artist2 = Artist.new({
+  'name' => 'Marvin Gaye'
+  })
+artist2.save()
+
+
+album1 = Album.new({
+  'title' => 'Midnight Organ Fight',
+  'genre' => 'Indie Rock',
+  'artist_id' => artist1.id
+  })
+album1.save()
+
+album2 = Album.new({
+  'title' => 'What\'s goin on',
+  'genre' => 'Indie Rock',
+  'artist_id' => artist2.id
+  })
+album2.save()
