c49113d986f0bc7ab96cad471344ed6a0ed4d75b
Gordon Soutter <gordon.soutter@outlook.com>
2020-03-25 21:34:18 +0000

    adds save to artist and albums. fixes error in sql runner

--- db/sql_runner.rb
+++ db/sql_runner.rb
@@ -1,18 +1,14 @@
 require('pg')
 
-def SqlRunner
-
+class SqlRunner
   def SqlRunner.run(sql, values)
     begin
-      db = PG.connect(database: 'music_collection', host: 'localhost')
-
+      db = PG.connect({dbname: 'music_collection', host: 'localhost'})
       db.prepare("query", sql)
       result =  db.exec_prepared("query", values)
     ensure
-      db.close
+      db.close()
     end
-
     return result
-
   end
 end
--- models/album.rb
+++ models/album.rb
@@ -7,7 +7,7 @@ class Album
   attr_reader :artist_id
 
   def initialize(options)
-    @id = options['id'].to_i if option['id']
+    @id = options['id'].to_i if options['id']
     @title = options['title']
     @genre = options['genre']
     @artist_id = options['artist_id'].to_i
